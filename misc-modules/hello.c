50c2f72af6b35fe3b522096463674e092fcee33a
Trieu Huynh Pham Nhat <vikingtc4@gmail.com>
2023-09-19 11:33:01 +0700

    Update for assignment 7 part 2.

--- misc-modules/hello.c
+++ misc-modules/hello.c
@@ -7,12 +7,14 @@ MODULE_LICENSE("Dual BSD/GPL");
 
 static int hello_init(void)
 {
+	printk(KERN_ALERT "hpntrieu\n");
 	printk(KERN_ALERT "Hello, world\n");
 	return 0;
 }
 
 static void hello_exit(void)
 {
+	printk(KERN_ALERT "hpntrieu\n");
 	printk(KERN_ALERT "Goodbye, cruel world\n");
 }
 
